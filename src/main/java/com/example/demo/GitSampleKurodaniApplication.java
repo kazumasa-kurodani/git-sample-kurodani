df2e3f6c11320f70397b88a29058a0863b76c53c
kazumasa.kurodani <kazumasa.kurodani@rakus.co.jp>
2017-06-14 10:25:29 +0900

    Gitテスト

---
+++ src/main/java/com/example/demo/GitSampleKurodaniApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKurodaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKurodaniApplication.class, args);
+	}
+}
